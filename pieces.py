0d5488f3ef81153ab8369ab5c00b3cf952086c1c
octavio <octavio.delser@gmail.com>
2022-05-18 13:53:50 +0200

    finished drawing board

--- pieces.py
+++ pieces.py
@@ -2,10 +2,30 @@ from __future__ import annotations
 
 import abc
 import math
+import time
 from enum import Enum
 from typing import List
 import numpy as np
 from numpy.typing import NDArray
+from blessed import Terminal
+from functools import partial
+
+# python 2/3 compatibility, provide 'echo' function as an
+# alias for "print without newline and flush"
+try:
+    # pylint: disable=invalid-name
+    #         Invalid constant name "echo"
+    echo = partial(print, end='', flush=True)
+    echo(u'')
+except TypeError:
+    # TypeError: 'flush' is an invalid keyword argument for this function
+    import sys
+
+
+    def echo(text):
+        """Python 2 version of print(end='', flush=True)."""
+        sys.stdout.write(u'{0}'.format(text))
+        sys.stdout.flush()
 
 
 class Board:
@@ -18,7 +38,31 @@ class Board:
     def validate_pos(self, pos: NDArray[int, int]):
         return self.board.shape >= pos
 
+    def draw_square(self, out: Terminal, coord: NDArray[int, int], color: Color):
+        scale = np.array([5,10], dtype=int)
+        coord_scaled = scale * coord
+        # print(print(coord, coord_scaled))
+        for i in range(coord_scaled[0], coord_scaled[0] + scale[0]):
+            for j in range(coord_scaled[1], coord_scaled[1] + scale[1]):
+                echo(out.move_yx(i, j))
+                if color==Color.WHITE:
+                    echo(out.on_black(u' '))
+                else:
+                    echo(out.on_white(u' '))
 
+    def draw(self, out: Terminal):
+        color_bg = out.on_blue
+        echo(out.move_yx(1, 1))
+        echo(color_bg(out.clear))
+        for i in range(self.board.shape[0]):
+            for j in range(self.board.shape[1]):
+                if i%2 and not j%2 or not i%2 and j%2:
+                    self.draw_square(out, np.array([i, j], dtype=int), Color.WHITE)
+                else:
+                    self.draw_square(out, np.array([i, j], dtype=int), Color.BLACK)
+
+    def set_piece(self,piece:Piece):
+        pass
 class Color(Enum):
     WHITE = 1
     BLACK = 0
@@ -28,6 +72,8 @@ class Piece:
     def __init__(self, color: Color.WHITE, pos: NDArray[int, int]):
         self.color = color
         self.pos = pos
+
+
 
     def __hash__(self):
         return self.pos
@@ -109,3 +155,22 @@ class Rook(Piece):
 class King(Piece):
     def valid_moves(self, board: Board) -> List[NDArray[int, int]]:
         return free_diagonal_indices(board, self.pos, 1) + free_horizontal_indices(board, self.pos, 1)
+
+
+class Game:
+    def __init__(self, board=Board()):
+        self.out = Terminal()
+        self.board = board
+
+    def draw(self):
+        inp = None
+        with self.out.hidden_cursor(), self.out.cbreak(), self.out.location():
+            while inp not in (u'q', u'Q'):
+                # inp = self.out.inkey()
+                self.board.draw(self.out)
+                time.sleep(10)
+
+
+g = Game()
+g.draw()
+
